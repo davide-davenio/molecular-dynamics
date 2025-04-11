aea83d8d474544c79712e4990d8961a7a798709d
davide-davenio <davide.davenio@gmail.com>
2025-04-11 11:27:53 +0200

    Delete loadData.m

--- loadData.m
+++
@@ -1,7 +0,0 @@
-%loads data from input file if its in three column format
-function [x, y, z] = loadData(dataFile) 
-    data = load(dataFile);
-    x = data(:, 1);
-    y = data(:, 2);
-    z = data(:, 3);
-end 
