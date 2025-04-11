f9b0593a8fe593ac437eabe6b044b92bc4e9e090
davide-davenio <davide.davenio@gmail.com>
2025-04-11 11:26:00 +0200

    Delete writeXYZ.m

--- writeXYZ.m
+++
@@ -1,9 +0,0 @@
-function writeXYZ(fname,x,y,z, nat)
-    %saves positions for each timestep
-  
-    filesnap=fopen(fname,'w');
-    for i=1:nat
-        fprintf(filesnap, '%g %g %g\n', x(i),y(i),z(i) );
-    end
-    fclose(filesnap);
-end
